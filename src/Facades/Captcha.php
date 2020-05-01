b88f466998d3043c5c426d4be6ab97d904ed4ee1
Youngyezi <youngyezi@github.com>
2020-05-01 22:18:10 +0800

    support lumen 6x

--- src/Facades/Captcha.php
+++ src/Facades/Captcha.php
@@ -5,12 +5,15 @@ namespace Youngyezi\Captcha\Facades;
 
 use Illuminate\Support\Facades\Facade;
 /**
- * @see \Mews\Captcha
+ * @see \Youngyezi\Captcha
  */
 class Captcha extends Facade
 {
     /**
      * @return string
      */
-    protected static function getFacadeAccessor() { return 'captcha'; }
+    protected static function getFacadeAccessor()
+    {
+        return 'captcha';
+    }
 }
